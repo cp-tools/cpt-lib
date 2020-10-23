15692e5cb5063ee762fbbfe420ec92ee61bee63a
Adithya Joshua Dsilva <adithyadsilva777@gmail.com>
2020-10-24 02:15:34 +0530

    Reduce panic issues, in page loading.
    * Return an error on failure to load page.

--- codeforces/problems.go
+++ codeforces/problems.go
@@ -86,7 +86,10 @@ func (arg Args) GetProblems() ([]Problem, error) {
 		return nil, err
 	}
 
-	page, msg := loadPage(link, `.problemindexholder`)
+	page, msg, err := loadPage(link, `.problemindexholder`)
+	if err != nil {
+		return nil, err
+	}
 	defer page.Close()
 
 	if msg != "" {
@@ -153,7 +156,11 @@ func (arg Args) SubmitSolution(langName string, file string) (<-chan Submission,
 		return nil, err
 	}
 
-	page, msg := loadPage(link, selCSSFooter)
+	page, msg, err := loadPage(link, selCSSFooter)
+	if err != nil {
+		return nil, err
+	}
+
 	if msg != "" {
 		defer page.Close()
 		return nil, fmt.Errorf(msg)
